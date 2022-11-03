24cda172dc9a0964bea16d2f46a5079413a6416d
Jamie Furlong <jamiefurlong16@gmail.com>
2022-11-03 17:14:50 +0000

    Fixed Menu Links & Setup Contact Form Feedback
    Alert now pops up when users submit contact form.
    Popup menu links now go to different sections of website.

--- main.js
+++ main.js
@@ -3,12 +3,22 @@ window.onload = function() {
         event.preventDefault();
         // these IDs from the previous steps
         emailjs.sendForm('portfolio_inquiry', 'contact_form', this)
-            .then(function() {
-                console.log('SUCCESS!');
-            }, function(error) {
-                console.log('FAILED...', error);
-            });
-
+        .then(function() {
+            console.log('SUCCESS!');
+            alertComplete();
+            clearForm();
+        }, function(error) {
+            console.log('FAILED...', error);
+        });
+        
+        function alertComplete() {
+            alert('Form submitted successfully!');
+        }
+        function clearForm() {
+            document.getElementById('name').value = '';
+            document.getElementById('email').value = '';
+            document.getElementById('query').value = '';
+        }
             // Add function to clear the email form and provide feedback to user that the form was submitted successfully!
     });
 }
